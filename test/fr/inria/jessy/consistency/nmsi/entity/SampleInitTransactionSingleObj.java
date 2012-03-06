59eb7c85102893e330b8f673b0c5c9277c601c18
msaeida <msaeida@334080fa-30e0-4884-a663-39494f8d70c4>
2012-03-06 10:51:38 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/regal/trunk/src/jessy@650 334080fa-30e0-4884-a663-39494f8d70c4

---
+++ test/fr/inria/jessy/consistency/nmsi/entity/SampleInitTransactionSingleObj.java
@@ -0,0 +1,29 @@
+package fr.inria.jessy.consistency.nmsi.entity;
+
+import fr.inria.jessy.Jessy;
+import fr.inria.jessy.store.Sample2EntityClass;
+import fr.inria.jessy.store.SampleEntityClass;
+import fr.inria.jessy.transaction.*;
+
+public class SampleInitTransactionSingleObj extends Transaction {
+
+	public SampleInitTransactionSingleObj(Jessy jessy) throws Exception{
+		super(jessy);
+	}
+
+	@Override
+	public ExecutionHistory execute() {
+		try {
+				
+			
+			SampleEntityClass se=new SampleEntityClass("1", "sampleentity1_INITIAL");			
+			write(se);
+			
+			return commitTransaction();			
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return null;
+		}		
+	}
+
+}
---
+++ test/fr/inria/jessy/consistency/nmsi/entity/SampleTransactionSingleObj1.java
@@ -0,0 +1,34 @@
+package fr.inria.jessy.consistency.nmsi.entity;
+
+import fr.inria.jessy.Jessy;
+import fr.inria.jessy.store.Sample2EntityClass;
+import fr.inria.jessy.store.SampleEntityClass;
+import fr.inria.jessy.transaction.*;
+
+public class SampleTransactionSingleObj1 extends Transaction {
+
+	public SampleTransactionSingleObj1(Jessy jessy) throws Exception{
+		super(jessy);
+	}
+
+	@Override
+	public ExecutionHistory execute() {
+		try {
+	
+			Thread.sleep(2000);
+			
+			SampleEntityClass se=read(SampleEntityClass.class, "1");			
+			se.setData("Second Trans");
+			write(se);
+			
+			
+			return commitTransaction();			
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return null;
+		}		
+	}
+
+}
+
+ 
