34dbc9c1d73e291856051194f1c09f9e905f4922
msaeida <msaeida@334080fa-30e0-4884-a663-39494f8d70c4>
2012-03-30 17:57:32 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/regal/trunk/src/jessy@882 334080fa-30e0-4884-a663-39494f8d70c4

--- src/fr/inria/jessy/transaction/termination/DistributedTermination.java
+++ src/fr/inria/jessy/transaction/termination/DistributedTermination.java
@@ -2,11 +2,12 @@ package fr.inria.jessy.transaction.termination;
 
 import java.io.Serializable;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
 
@@ -23,11 +24,12 @@ import fr.inria.jessy.Jessy;
 import fr.inria.jessy.Partitioner;
 import fr.inria.jessy.consistency.ConsistencyFactory;
 import fr.inria.jessy.transaction.ExecutionHistory;
-import fr.inria.jessy.transaction.ExecutionHistory.TransactionType;
+import fr.inria.jessy.transaction.ExecutionHistory.TransactionState;
 import fr.inria.jessy.transaction.TransactionHandler;
 
 //TODO COMMENT ME
 //TODO GARBAGE COLLECT ME
+//TODO JESSY define pattern is crappy!
 public class DistributedTermination implements Learner, Runnable {
 
 	private static DistributedTermination instance;
@@ -64,8 +66,8 @@ public class DistributedTermination implements Learner, Runnable {
 	private ConcurrentMap<TransactionHandler, TerminationResult> terminationResults;
 	private ConcurrentMap<TransactionHandler, VotingQuorum> votingQuorums;
 
-	// TODO this might be costy. Can't we just simply use an arraylist?
-	private LinkedBlockingQueue<TerminateTransactionMessage> terminateTransactionMessages;
+	private BlockingQueue<TerminateTransactionMessage> atomicDeliveredMessages;
+	private CopyOnWriteArrayList<TerminateTransactionMessage> processingMessages;
 
 	private String myGroup = Membership.getInstance().myGroup().name();
 
@@ -83,13 +85,15 @@ public class DistributedTermination implements Learner, Runnable {
 
 		futures = new ConcurrentHashMap<TransactionHandler, Future<TerminationResult>>();
 		terminationResults = new ConcurrentHashMap<TransactionHandler, TerminationResult>();
-		terminateTransactionMessages = new LinkedBlockingQueue<TerminateTransactionMessage>();
+		atomicDeliveredMessages = new LinkedBlockingQueue<TerminateTransactionMessage>();
+		processingMessages = new CopyOnWriteArrayList<DistributedTermination.TerminateTransactionMessage>();
 		votingQuorums = new ConcurrentHashMap<TransactionHandler, VotingQuorum>();
 	}
 
 	public Future<TerminationResult> terminateTransaction(ExecutionHistory ex) {
-		Future<TerminationResult> reply = pool
-				.submit(new TerminateTransactionTask(ex));
+		ex.changeState(TransactionState.COMMITTING);
+		Future<TerminationResult> reply = pool.submit(new AtomicMulticastTask(
+				ex));
 		futures.put(ex.getTransactionHandler(), reply);
 		return reply;
 	}
@@ -97,7 +101,7 @@ public class DistributedTermination implements Learner, Runnable {
 	@Deprecated
 	public void learn(Stream s, Serializable v) {
 		if (v instanceof TerminateTransactionMessage) {
-			terminateTransactionMessages.add((TerminateTransactionMessage) v);
+			atomicDeliveredMessages.add((TerminateTransactionMessage) v);
 
 		} else {
 			addVote((Vote) v);
@@ -108,14 +112,144 @@ public class DistributedTermination implements Learner, Runnable {
 	@Override
 	public void run() {
 		TerminateTransactionMessage msg;
-		Boolean outcome;
+
 		while (true) {
 
-			msg = terminateTransactionMessages.poll();
+			msg = atomicDeliveredMessages.peek();
 			if (msg == null)
 				continue;
 
-			outcome = ConsistencyFactory.getConsistency()
+			for (TerminateTransactionMessage committing : processingMessages) {
+				if (ConsistencyFactory.getConsistency().hasConflict(
+						msg.getExecutionHistory(),
+						committing.getExecutionHistory())) {
+					continue;
+				}
+			}
+
+			msg = atomicDeliveredMessages.poll();
+			processingMessages.add(msg);
+			pool.submit(new CertifyAndVoteTask(msg));
+
+		}
+
+	}
+
+	private void addVote(Vote vote) {
+		if (votingQuorums.containsKey(vote.getTransactionHandler())) {
+			votingQuorums.get(vote.getTransactionHandler()).addVote(vote);
+		} else {
+			VotingQuorum vq = new VotingQuorum(vote.getTransactionHandler(),
+					vote.getAllVoterGroups());
+			vq.addVote(vote);
+			votingQuorums.put(vote.getTransactionHandler(), vq);
+		}
+	}
+
+	private synchronized void notifyFuture(TerminationResult terminationResult,
+			TransactionHandler transactionHandler, boolean certifyAtProxy) {
+		Future<TerminationResult> future = futures.get(transactionHandler);
+		if (future != null) {
+			terminationResults.put(transactionHandler, terminationResult);
+			future.notify();
+		} else if (!certifyAtProxy) {
+			// TODO SEND the result back to the proxy, otherwise, it will
+			// receive it itself.
+		}
+
+		/*
+		 * TODO Garbage collect here!
+		 */
+	}
+
+	public class VoteMessage extends WanMessage {
+		private static final long serialVersionUID = ConstantPool.JESSY_MID;
+
+		// For Fractal
+		public VoteMessage() {
+		}
+
+		VoteMessage(Vote vote, Set<String> dest) {
+			super(vote, dest, Membership.getInstance().myGroup().name(),
+					Membership.getInstance().myId());
+		}
+
+		public Vote getVote() {
+			return (Vote) serializable;
+		}
+
+	}
+
+	public class TerminateTransactionMessage extends WanMessage {
+		private static final long serialVersionUID = ConstantPool.JESSY_MID;
+		ExecutionHistory executionHistory;
+
+		// For Fractal
+		public TerminateTransactionMessage() {
+		}
+
+		TerminateTransactionMessage(ExecutionHistory eh, Set<String> dest) {
+			super(eh, dest, myGroup, Membership.getInstance().myId());
+		}
+
+		public ExecutionHistory getExecutionHistory() {
+			return executionHistory;
+		}
+
+	}
+
+	private class AtomicMulticastTask implements Callable<TerminationResult> {
+
+		private ExecutionHistory executionHistory;
+
+		private AtomicMulticastTask(ExecutionHistory eh) {
+			this.executionHistory = eh;
+		}
+
+		public TerminationResult call() throws Exception {
+			HashSet<String> destGroups = new HashSet<String>();
+			Set<String> concernedKeys = ConsistencyFactory
+					.getConcerningKeys(executionHistory);
+
+			/*
+			 * If there is no concerningkey, it means that the transaction can
+			 * commit right away. e.g. read-only transaction with NMSI
+			 * consistency.
+			 */
+			if (concernedKeys.size() == 0)
+				return TerminationResult.COMMITTED;
+
+			destGroups.addAll(Partitioner.getInstance().resolveToGroupNames(
+					concernedKeys));
+
+			/*
+			 * if this process will receive this transaction through atomic
+			 * multicast, certifyAtProxy is set to true, otherwise, it is set to
+			 * false.
+			 */
+			if (destGroups.contains(myGroup))
+				executionHistory.setCertifyAtProxy(true);
+			else
+				executionHistory.setCertifyAtProxy(false);
+
+			amStream.atomicMulticast(new TerminateTransactionMessage(
+					executionHistory, destGroups), destGroups);
+			futures.get(executionHistory.getTransactionHandler()).wait();
+			return terminationResults.get(executionHistory
+					.getTransactionHandler());
+		}
+	}
+
+	class CertifyAndVoteTask implements Callable<Boolean> {
+
+		private TerminateTransactionMessage msg;
+
+		private CertifyAndVoteTask(TerminateTransactionMessage msg) {
+			this.msg = msg;
+		}
+
+		public Boolean call() throws Exception {
+			boolean outcome = ConsistencyFactory.getConsistency()
 					.certify(jessy.getLastCommittedEntities(),
 							msg.getExecutionHistory());
 
@@ -168,119 +302,9 @@ public class DistributedTermination implements Learner, Runnable {
 				notifyFuture(result, msg.getExecutionHistory()
 						.getTransactionHandler(), msg.getExecutionHistory()
 						.isCertifyAtProxy());
+
 			}
-
-		}
-
-	}
-
-	private void addVote(Vote vote) {
-		if (votingQuorums.containsKey(vote.getTransactionHandler())) {
-			votingQuorums.get(vote.getTransactionHandler()).addVote(vote);
-		} else {
-			VotingQuorum vq = new VotingQuorum(vote.getTransactionHandler(),
-					vote.getAllVoterGroups());
-			vq.addVote(vote);
-			votingQuorums.put(vote.getTransactionHandler(), vq);
+			return true;
 		}
 	}
-
-	private void notifyFuture(TerminationResult terminationResult,
-			TransactionHandler transactionHandler, boolean certifyAtProxy) {
-		Future<TerminationResult> future = futures.get(transactionHandler);
-		if (future != null) {
-			terminationResults.put(transactionHandler, terminationResult);
-			future.notify();
-		} else if (!certifyAtProxy) {
-			// TODO SEND the result back to the proxy, otherwise, it will
-			// receive it itself.
-		}
-		
-		/*
-		 * TODO Garbage collect here!
-		 */
-	}
-
-	public class VoteMessage extends WanMessage {
-		private static final long serialVersionUID = ConstantPool.JESSY_MID;
-
-		// For Fractal
-		public VoteMessage() {
-		}
-
-		VoteMessage(Vote vote, Set<String> dest) {
-			super(vote, dest, Membership.getInstance().myGroup().name(),
-					Membership.getInstance().myId());
-		}
-
-		public Vote getVote() {
-			return (Vote) serializable;
-		}
-
-	}
-
-	public class TerminateTransactionMessage extends WanMessage {
-		private static final long serialVersionUID = ConstantPool.JESSY_MID;
-		ExecutionHistory executionHistory;
-
-		// For Fractal
-		public TerminateTransactionMessage() {
-		}
-
-		TerminateTransactionMessage(ExecutionHistory eh, Set<String> dest) {
-			super(eh, dest, myGroup, Membership.getInstance().myId());
-		}
-
-		public ExecutionHistory getExecutionHistory() {
-			return executionHistory;
-		}
-
-	}
-
-	class TerminateTransactionTask implements Callable<TerminationResult> {
-
-		private ExecutionHistory executionHistory;
-
-		public ExecutionHistory getExecutionHistory() {
-			return executionHistory;
-		}
-
-		private TerminateTransactionTask(ExecutionHistory eh) {
-			this.executionHistory = eh;
-		}
-
-		public TerminationResult call() throws Exception {
-			HashSet<String> destGroups = new HashSet<String>();
-			Set<String> concernedKeys = ConsistencyFactory
-					.getConcerningKeys(executionHistory);
-
-			/*
-			 * If there is no concerningkey, it means that the transaction can
-			 * commit right away. e.g. read-only transaction with NMSI
-			 * consistency.
-			 */
-			if (concernedKeys.size() == 0)
-				return TerminationResult.COMMITTED;
-
-			destGroups.addAll(Partitioner.getInstance().resolveToGroupNames(
-					concernedKeys));
-
-			/*
-			 * if this process will receive this transaction through atomic
-			 * multicast, certifyAtProxy is set to true, otherwise, it is set to
-			 * false.
-			 */
-			if (destGroups.contains(myGroup))
-				executionHistory.setCertifyAtProxy(true);
-			else
-				executionHistory.setCertifyAtProxy(false);
-
-			amStream.atomicMulticast(new TerminateTransactionMessage(
-					executionHistory, destGroups), destGroups);
-			futures.get(executionHistory.getTransactionHandler()).wait();
-			return terminationResults.get(executionHistory
-					.getTransactionHandler());
-		}
-	}
-
 }
