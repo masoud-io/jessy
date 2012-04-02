e26feeeed6032e2e570659e4aad6dfb032ae6289
msaeida <msaeida@334080fa-30e0-4884-a663-39494f8d70c4>
2012-04-02 17:56:23 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/regal/trunk/src/jessy@948 334080fa-30e0-4884-a663-39494f8d70c4

---
+++ test/fr/inria/jessy/distributed/nmsi/transaction/SampleTransactionMultiObj2.java
@@ -0,0 +1,39 @@
+package fr.inria.jessy.distributed.nmsi.transaction;
+
+import fr.inria.jessy.Jessy;
+import fr.inria.jessy.entity.Sample2EntityClass;
+import fr.inria.jessy.entity.SampleEntityClass;
+import fr.inria.jessy.transaction.ExecutionHistory;
+import fr.inria.jessy.transaction.Transaction;
+
+public class SampleTransactionMultiObj2 extends Transaction {
+
+	public SampleTransactionMultiObj2(Jessy jessy) throws Exception{
+		super(jessy);
+	}
+
+	@Override
+	public ExecutionHistory execute() {
+		try {
+	
+			Thread.sleep(500);
+			
+			SampleEntityClass se2=read(SampleEntityClass.class, "1");
+			
+			Sample2EntityClass se=read(Sample2EntityClass.class, "1");			
+			
+			se.setData("Second Trans");
+			write(se);
+			
+			Thread.sleep(3000);
+			
+			return commitTransaction();			
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return null;
+		}		
+	}
+
+}
+
+ 
---
+++ test/fr/inria/jessy/distributed/nmsi/transaction/SampleTransactionMultiObj3.java
@@ -0,0 +1,34 @@
+package fr.inria.jessy.distributed.nmsi.transaction;
+
+import fr.inria.jessy.Jessy;
+import fr.inria.jessy.entity.Sample2EntityClass;
+import fr.inria.jessy.transaction.ExecutionHistory;
+import fr.inria.jessy.transaction.Transaction;
+
+public class SampleTransactionMultiObj3 extends Transaction {
+
+	public SampleTransactionMultiObj3(Jessy jessy) throws Exception{
+		super(jessy);
+	}
+
+	@Override
+	public ExecutionHistory execute() {
+		try {
+	
+			Thread.sleep(2000);
+			
+			Sample2EntityClass se=read(Sample2EntityClass.class, "1");			
+			se.setData("Second Trans");
+			write(se);
+			
+			
+			return commitTransaction();			
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return null;
+		}		
+	}
+
+}
+
+ 
