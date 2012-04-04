653f74a79b7caf4c89f9b245b88aa64bc6a08a14
kriseuna <kriseuna@334080fa-30e0-4884-a663-39494f8d70c4>
2012-04-04 21:02:18 +0000

    test of transactions
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/regal/trunk/src/jessy@964 334080fa-30e0-4884-a663-39494f8d70c4

---
+++ test/fr/inria/jessy/benchmark/tpcc/TpccTestPayment.java
@@ -0,0 +1,67 @@
+package fr.inria.jessy.benchmark.tpcc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import fr.inria.jessy.LocalJessy;
+import fr.inria.jessy.benchmark.tpcc.entities.*;
+import fr.inria.jessy.transaction.*;
+
+/**
+ * @author WANG Haiyun & ZHAO Guang
+ * 
+ */
+
+public class TpccTestPayment {
+	
+	LocalJessy jessy;
+	InsertData id;
+	Payment payment; 
+
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		jessy = LocalJessy.getInstance();
+
+		jessy.addEntity(Warehouse.class);
+		jessy.addEntity(District.class);
+		jessy.addEntity(Customer.class);
+		jessy.addEntity(Item.class);
+		jessy.addEntity(Stock.class);
+		jessy.addEntity(History.class);
+		jessy.addEntity(Order.class);
+		jessy.addEntity(New_order.class);
+		jessy.addEntity(Order_line.class);
+		id = new InsertData(jessy);
+		payment = new Payment(jessy);
+		id.execute();
+	}
+	
+	/**
+	 * Test method for {@link fr.inria.jessy.BenchmarkTpcc.InsertData}.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testPayment() throws Exception {
+
+		ExecutionHistory result = payment.execute();
+		/* test execution */
+		assertEquals("Result", TransactionState.COMMITTED,
+				result.getTransactionState());			
+
+	}
+
+}
