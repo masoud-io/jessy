1cbb14ddc9d0d10eb3275f35f4c13d4aa7d12dda
msaeida <msaeida@334080fa-30e0-4884-a663-39494f8d70c4>
2012-04-12 13:57:35 +0000

    commented
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/regal/trunk/src/jessy@999 334080fa-30e0-4884-a663-39494f8d70c4

--- src/fr/inria/jessy/transaction/termination/VotingQuorum.java
+++ src/fr/inria/jessy/transaction/termination/VotingQuorum.java
@@ -6,7 +6,21 @@ import java.util.concurrent.CopyOnWriteArraySet;
 import fr.inria.jessy.transaction.TransactionHandler;
 import fr.inria.jessy.transaction.TransactionState;
 
-//TODO COMMENT ME
+/**
+ * This class receives {@link Vote} from different participations in the
+ * transaction termination phase. It returns {@link TransactionState.COMMITTED}
+ * if it receives <code>true</code> votes from a member of all groups concerned
+ * by the transaction. If it receives a <code>false</code> vote from a member of
+ * a group, it will returns {@link TransactionState.ABORTED_BY_VOTING}.
+ * Otherwise, it will simply waits until receiving enough votes.
+ * <p>
+ * A set of concerned groups is provided by {@code groups} argument of the
+ * constructor.
+ * 
+ * @author Masoud Saeida Ardekani
+ * @author Pierre Sutra
+ * 
+ */
 public class VotingQuorum {
 
 	TransactionState result = TransactionState.COMMITTING;
@@ -21,27 +35,44 @@ public class VotingQuorum {
 		receivedVotes.addAll(groups);
 	}
 
+	/**
+	 * Add a received {@code vote}.
+	 * <p>
+	 * It returns {@link TransactionState.COMMITTED} if it receives
+	 * <code>true</code> votes from a member of all groups concerned by the
+	 * transaction. If it receives a <code>false</code> vote from a member of a
+	 * group, it will returns {@link TransactionState.ABORTED_BY_VOTING}.
+	 * 
+	 * @param vote
+	 */
 	public synchronized void addVote(Vote vote) {
 		if (vote.isCertified() == false) {
 			result = TransactionState.ABORTED_BY_VOTING;
-			synchronized(this){
+			synchronized (this) {
 				notify();
 			}
 		} else {
 			receivedVotes.remove(vote.getGroupName());
 			if (receivedVotes.size() == 0) {
 				result = TransactionState.COMMITTED;
-				synchronized(this){
+				synchronized (this) {
 					notify();
 				}
 			}
 		}
 	}
 
+	/**
+	 * It simply waits until it receives enough votes from members of all groups
+	 * concerned by the transaction.
+	 * 
+	 * @return either {@link TransactionState.COMMITTED} or
+	 *         {@link TransactionState.ABORTED_BY_VOTING}
+	 */
 	public TransactionState getTerminationResult() {
 		try {
 			if (result == TransactionState.COMMITTING)
-				synchronized(this){
+				synchronized (this) {
 					wait();
 				}
 		} catch (InterruptedException e) {
