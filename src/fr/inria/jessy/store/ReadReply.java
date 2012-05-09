7deeba02c5fd984b7d47651e58ddc342dfad91d8
msaeida <msaeida@334080fa-30e0-4884-a663-39494f8d70c4>
2012-05-09 17:31:08 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/regal/trunk/src/jessy@1066 334080fa-30e0-4884-a663-39494f8d70c4

--- src/fr/inria/jessy/store/ReadReply.java
+++ src/fr/inria/jessy/store/ReadReply.java
@@ -1,18 +1,29 @@
 package fr.inria.jessy.store;
 
-import java.io.Serializable;
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.UUID;
 
 import fr.inria.jessy.ConstantPool;
 
-public class ReadReply<E extends JessyEntity> implements Serializable {
-	
+public class ReadReply<E extends JessyEntity> implements Externalizable {
+
 	private static final long serialVersionUID = ConstantPool.JESSY_MID;
 
 	UUID readRequestId;
 	Collection<E> entities;
+
+	/**
+	 * For externalizable interface
+	 */
+	@Deprecated
+	public ReadReply() {
+
+	}
 
 	public ReadReply(E entity, UUID correspondingReadRequestId) {
 		entities = new ArrayList<E>();
@@ -34,9 +45,35 @@ public class ReadReply<E extends JessyEntity> implements Serializable {
 	}
 
 	public synchronized void mergeReply(ReadReply<E> readReply) {
-		if ( !this.readRequestId.equals(readReply.getReadRequestId()) )
+		if (!this.readRequestId.equals(readReply.getReadRequestId()))
 			throw new IllegalArgumentException("Invalid requestId");
 		entities.addAll(readReply.getEntity());
+	}
+
+	@Override
+	public void writeExternal(ObjectOutput out) throws IOException {
+		out.writeObject(readRequestId);
+		// out.writeObject(entities);
+		if (entities.size() == 1) {
+			out.writeBoolean(true);
+			out.writeObject(entities.iterator().next());
+		} else {
+			out.writeBoolean(false);
+			out.writeObject(entities);
+		}
+	}
+
+	@Override
+	public void readExternal(ObjectInput in) throws IOException,
+			ClassNotFoundException {
+		readRequestId = (UUID) in.readObject();
+		// entities = (Collection<E>) in.readObject();
+		if (in.readBoolean()) {
+			entities = new ArrayList<E>();
+			entities.add((E) in.readObject());
+		} else {
+			entities = (Collection<E>) in.readObject();
+		}
 	}
 
 }
