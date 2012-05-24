dc0bce6b2f2992f774d7659318f19c32c2dac62c
msaeida <msaeida@334080fa-30e0-4884-a663-39494f8d70c4>
2012-05-24 16:40:24 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/regal/trunk/src/jessy@1137 334080fa-30e0-4884-a663-39494f8d70c4

--- test/fr/inria/jessy/local/ser/transaction/SampleTransactionMultiObj3.java
+++ test/fr/inria/jessy/local/ser/transaction/SampleTransactionMultiObj3.java
@@ -1,30 +1,28 @@
-package fr.inria.jessy.local.si.transaction;
+package fr.inria.jessy.local.ser.transaction;
 
 import fr.inria.jessy.Jessy;
 import fr.inria.jessy.entity.Sample2EntityClass;
-import fr.inria.jessy.entity.SampleEntityClass;
 import fr.inria.jessy.transaction.ExecutionHistory;
 import fr.inria.jessy.transaction.Transaction;
 
-public class T1 extends Transaction{
+public class SampleTransactionMultiObj3 extends Transaction {
 
-	public T1(Jessy jessy) throws Exception{
+	public SampleTransactionMultiObj3(Jessy jessy) throws Exception{
 		super(jessy);
-		setRetryCommitOnAbort(false);
 	}
-	
+
 	@Override
 	public ExecutionHistory execute() {
-
 		try {
-			
-			SampleEntityClass en=read(SampleEntityClass.class, "1");
+	
 			Thread.sleep(2000);
 			
-			Sample2EntityClass en2=read(Sample2EntityClass.class, "2");
+			Sample2EntityClass se=read(Sample2EntityClass.class, "2");			
+			se.setData("Second Trans");
+			write(se);
 			
-			return commitTransaction();	
 			
+			return commitTransaction();			
 		} catch (Exception ex) {
 			ex.printStackTrace();
 			return null;
@@ -32,3 +30,5 @@ public class T1 extends Transaction{
 	}
 
 }
+
+ 
--- test/fr/inria/jessy/local/ser/transaction/SampleTransactionMultiObj4.java
+++ test/fr/inria/jessy/local/ser/transaction/SampleTransactionMultiObj4.java
@@ -1,4 +1,4 @@
-package fr.inria.jessy.local.si.transaction;
+package fr.inria.jessy.local.ser.transaction;
 
 import fr.inria.jessy.Jessy;
 import fr.inria.jessy.entity.Sample2EntityClass;
@@ -6,25 +6,26 @@ import fr.inria.jessy.entity.SampleEntityClass;
 import fr.inria.jessy.transaction.ExecutionHistory;
 import fr.inria.jessy.transaction.Transaction;
 
-public class T1 extends Transaction{
+public class SampleTransactionMultiObj4 extends Transaction {
 
-	public T1(Jessy jessy) throws Exception{
+	public SampleTransactionMultiObj4(Jessy jessy) throws Exception{
 		super(jessy);
-		setRetryCommitOnAbort(false);
 	}
-	
+
 	@Override
 	public ExecutionHistory execute() {
-
 		try {
+	
+			Thread.sleep(500);
 			
-			SampleEntityClass en=read(SampleEntityClass.class, "1");
-			Thread.sleep(2000);
+			SampleEntityClass se=read(SampleEntityClass.class, "1");			
 			
-			Sample2EntityClass en2=read(Sample2EntityClass.class, "2");
+			Thread.sleep(4000);
 			
-			return commitTransaction();	
+			Sample2EntityClass se2=read(Sample2EntityClass.class, "2");
 			
+			
+			return commitTransaction();			
 		} catch (Exception ex) {
 			ex.printStackTrace();
 			return null;
@@ -32,3 +33,5 @@ public class T1 extends Transaction{
 	}
 
 }
+
+ 
