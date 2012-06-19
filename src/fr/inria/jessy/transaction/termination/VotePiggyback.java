77b086beaf12b7a4cfadb6333cb6f94245317bb2
msaeida <msaeida@334080fa-30e0-4884-a663-39494f8d70c4>
2012-06-19 14:35:38 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/regal/trunk/src/jessy@1203 334080fa-30e0-4884-a663-39494f8d70c4

---
+++ src/fr/inria/jessy/transaction/termination/VotePiggyback.java
@@ -0,0 +1,11 @@
+package fr.inria.jessy.transaction.termination;
+
+import java.io.Externalizable;
+
+public abstract class VotePiggyback implements Externalizable{
+
+	public VotePiggyback(){
+		
+	}
+
+}
