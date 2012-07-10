ab95dd13cf4bf191014c9d76ad086e5413dba5bb
cincilla <cincilla@334080fa-30e0-4884-a663-39494f8d70c4>
2012-07-10 15:39:13 +0000

    single-cluster launcher for grid5000. Done (the default is nancy cluster)
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/regal/trunk/src/jessy@1365 334080fa-30e0-4884-a663-39494f8d70c4

--- script/grid5kLauncher.sh
+++ script/grid5kLauncher.sh
@@ -1,10 +1,23 @@
+#!/bin/bash
+
 if [ ${#} -lt 2 ]
 then
   echo 'grid5kLaucher: usage: this script need at least two parameters: number of clients and number of servers'
   exit
 fi
 
+trap "stopExecution"  SIGINT SIGTERM
+
 path=$(pwd)
+
+function stopExecution(){
+        
+	echo "grid5kLaucher: grid5kLaucher stop. Deleting job..."
+	oargriddel $RES_ID
+        echo "grid5kLaucher: done"
+
+        exit
+}
 
 sed -ie "s#^source.*#source $path/configuration.sh#g" clauncher.sh
 sed -ie "s#^source.*#source $path/configuration.sh#g" client.sh 
@@ -18,27 +31,31 @@ clientsNumber=$1
 serversNumber=$2
 nodesNumber=$((clientsNumber+serversNumber))
 
-#echo 'clientsNumber:'$clientsNumber
-#echo 'serversNumber':$serversNumber
-#echo 'nodesNumber':$nodesNumber
+#clientsNumber=2
+#serversNumber=2
+#nodesNumber=4
+
 
 echo "grid5kLaucher: reserving $nodesNumber nodes..."
-oargridsub -t allow_classic_ssh -w '24:00:00' nancy:rdef="/nodes=$nodesNumber" > tmp
+oargridsub -t allow_classic_ssh -w '01:00:00' nancy:rdef="/nodes=$nodesNumber" > tmp
+#oargridsub -t allow_classic_ssh -w '00:04:00' nancy:rdef="/nodes=4" > tmp
+#oargridsub -t allow_classic_ssh -w '0:20:00' nancy:rdef="/nodes=2", lille:rdef="/nodes=2" > tmp
 echo "grid5kLaucher: done"
 
-#echo "retreving batch and grid reservation IDs..."
+#retreving batch and grid reservation IDs
 RES_ID=$(grep "Grid reservation id" tmp | cut -f2 -d=)
-BATCH_ID=$(grep "batchId" tmp | cut -f2 -d=)
-BATCH_ID=${BATCH_ID//[[:space:]]/}
+OAR_JOB_KEY_PATH=$(grep "SSH KEY" tmp | cut -b 25-)
+#BATCH_ID=$(grep "batchId" tmp | cut -f2 -s -d=)
+#BATCH_ID=${BATCH_ID//[[:space:]]/}
 
-#echo 'RES_ID='$RES_ID
-#echo 'BATCH_ID='$BATCH_ID
+#writing names and ip in ./machines file
+oargridstat -w -l $RES_ID | sed '/^$/d' | sort | uniq > ./machines
 
-#echo 'writing names and ip in ./machines file'
-oargridstat -w -l $RES_ID | sed '/^$/d' | uniq > ./machines
+#echo "sleeping 60 sec"
+#sleep 60
+#echo "getUp"
 
-#echo 'grid5kLaucher: DELETING JOBS! only for testing pourpose'
-#oardel $BATCH_ID
+
 rm myfractal.xml
 
 echo '<?xml version="1.0" encoding="ISO-8859-1" ?>' >> myfractal.xml
@@ -50,13 +67,14 @@ nodes='' #'nodes=('
 servers='' #'servers=('
 clients='' #'clients=('
 
+echo ''
+
 i=0
 while read line
 do
   host $line > tmp
   name=$(cut tmp -f1 -d ' ')
   ip=$(cut tmp -f4 -d ' ')
-#  echo '<node id="'$i'" ip="'$ip'"/>' >> myfractal.xml
  
   nodes="$nodes \"$name\""
   if [ $i -lt $serversNumber ]
@@ -69,24 +87,18 @@ do
     clients="$clients \"$name\""
   fi
 
-  
-#   echo 'rsync --delete -avz ~/.ssh --exclude known_hosts  $name:'
-#   rsync --delete -avz ~/.ssh --exclude known_hosts  $name:
-
-#   echo t4nt\&R0se | passwd theUsername --stdin
-
-#  echo $name $ip >> machinesNames
   i=$((i+1))
 done < machines
+
+echo ''
 
 nodes="nodes=("$nodes")"
 servers="servers=("$servers")"
 clients="clients=("$clients")"
 
-
-#echo 'nodes='$nodes
-#echo 'servers='$servers
-#echo 'clients='$clients
+#echo "sleeping 600 sec before remove machines and tmp"
+#sleep 600
+#echo "getUp"
 
 rm machines tmp
 
@@ -99,9 +111,23 @@ sed -i "s/nodes=.*/${nodes}/g" configuration.sh
 sed -i "s/servers=.*/${servers}/g" configuration.sh
 sed -i "s/clients=.*/${clients}/g" configuration.sh
 
-export OAR_JOB_ID=$BATCH_ID
+#export OAR_JOB_ID=$BATCH_ID
+export OAR_JOB_KEY_FILE=$OAR_JOB_KEY_PATH
 
-echo "grid5kLaucher: myfractal and configuration are done, lauching experience..."
+echo 'grid5kLaucher: exported oarJobKeyFile ' $OAR_JOB_KEY_PATH
+
+#echo 'grid5kLaucher: synchronizing keys and data...'
+#rsync --delete -avz ~/.ssh --exclude known_hosts lille.grid5000.fr:
+#rsync --delete -avz ./* lille.grid5000.fr:./jessy/scripts/
+#echo 'grid5kLaucher: done'
+
+#echo "sleeping 600 sec before run experience"
+#sleep 600
+#echo "getUp"
+
+
+echo "grid5kLaucher: myfractal and configuration.sh are done, lauching experience..."
 ./experience.sh
-echo "grid5kLaucher: done, deleting job $"
-oardel $BATCH_ID
+echo "grid5kLaucher: done, deleting jobs"
+
+oargriddel $RES_ID
