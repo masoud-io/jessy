d1a492113ebbd1a1581eb6fe1e28dbdd628989f4
msaeida <msaeida@334080fa-30e0-4884-a663-39494f8d70c4>
2012-07-19 22:13:23 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/regal/trunk/src/jessy@1592 334080fa-30e0-4884-a663-39494f8d70c4

--- script/configuration.sh
+++ script/configuration.sh
@@ -6,22 +6,32 @@ bc="/home/psutra/utils/bin/bc"
 scriptdir="/home/msaeida/jessy_script"
 workingdir="/tmp/jessy_exec"
 
-nodes=("cluster1u1" "cluster1u2" "cluster1u4" "cluster1u5" "cluster1u6" "cluster1u7" "cluster1u8" "cluster1u9" "cluster1u11" "cluster1u12") # "cluster1u14")
-# "cluster1u14" "cluster1u15" "cluster1u16") # "cluster1u17" "cluster1u18"  "cluster1u19" "cluster1u20" "cluster1u21" "cluster1u22" "cluster1u23" "cluster1u24")
-servers=("cluster1u1" "cluster1u2") # "cluster1u4" "cluster1u5")
-clients=("cluster1u7" "cluster1u8") # "cluster1u9" "cluster1u11")
+nodes=("cluster1u1" "cluster1u2" "cluster1u4" "cluster1u5" "cluster1u6" "cluster1u7" "cluster1u8" "cluster1u9" "cluster1u11" "cluster1u12" "cluster1u13" "cluster1u14" "cluster1u15" "cluster1u16" "cluster1u17" "cluster1u18"  "cluster1u19" "cluster1u20" "cluster1u21" "cluster1u22" "cluster1u23" "cluster1u24")
+servers=("cluster1u1")
+clients=("cluster1u11" "cluster1u12")
 
 # Experience.sh settings
 
-cons=("si2" "us" "nmsi" "psi" "rc" "ser")
-client_thread_increment="5"
-client_thread_glb="20"
-client_thread_lub="40"
+# servers
+server_machine_increment="1"
+server_machine_glb="1"
+server_machine_lub="2"
+
+# clients ( = client_machine * client_thread )
+client_machine_increment="1"
+client_machine_glb="1"
+client_machine_lub="3"
+client_thread_increment="1"
+client_thread_glb="3"
+client_thread_lub="3"
+
+# Consistency
+cons=("rc") #"si2" "psi" "nmsi" "rc" "us" "ser")
 
 # Client specific settings 
-workloadType="-t"
+workloadType="-load"
 workloadName="transactionalworkloada"
-nthreads=40
+nthreads=3
 
 system=jessy
 
---
+++ script/lip6cluster.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+source /home/msaeida/jessy_script/configuration.sh
+
+trap "kill 0; wait; exit 255" SIGINT SIGTERM
+
+# 2 - Configuring
+nvar=(`oarstat -f | grep assigned_hostnames | gawk -F= '{print $2}' | sed s/+/" "/g`)
+
+# 3 - Launching
+nservers=`seq ${server_machine_glb} ${server_machine_increment} ${server_machine_lub}`
+nclients=`seq ${client_machine_glb} ${client_machine_increment} ${client_machine_lub}`
+for s in ${nservers}; 
+do
+
+    # Construct servers variable.
+    svar="\"${nvar[0]}\""
+    for i in `seq 1 $[${s}-1]`
+    do
+	svar="${svar} \"${nvar[$i]}\""
+    done
+    svar="servers=(${svar})"
+    sed -i "s/servers=.*/${svar}/g" configuration.sh
+
+    # Generate the fractal XML file
+    echo '<?xml version="1.0" encoding="ISO-8859-1" ?>' > tmp.xml
+    echo '<FRACTAL>' >> tmp.xml
+    echo ' <BootstrapIdentity>' >> tmp.xml
+    echo '  <nodelist>' >> tmp.xml
+    for i in `seq 0 $[${s}-1]`
+    do
+	ip=`host ${nvar[$i]} | gawk -F" " '{print $4}'`
+	echo "   <node id=\"${i}\" ip=\"${ip}\"/>" >> tmp.xml
+    done
+    echo "  </nodelist>" >> tmp.xml
+    echo " </BootstrapIdentity>" >> tmp.xml
+    echo "</FRACTAL>" >> tmp.xml
+    mv tmp.xml myfractal.xml
+
+    for c in ${nclients};
+    do
+
+        # Construct clients variable.
+	let a=$[${i}+1]
+	cvar="\"${nvar[$a]}\""
+	for j in `seq $[${i}+2] $[${i}+${c}]`
+ 	do
+ 	    cvar="${cvar} \"${nvar[$j]}\""
+	done
+	cvar="clients=(${cvar})"
+        sed -i "s/clients=.*/${cvar}/g" configuration.sh
+
+	# Now, launching
+	echo "Launching with ${s} server(s) and ${c} client(s)"
+	${scriptdir}/experience.sh       
+	
+   done
+    
+done
+
