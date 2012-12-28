01261fafef965af973863a1f104b4184d40558c4
Masoud Saeida Ardekani <db.oracle@gmail.com>
2012-12-28 18:38:29 +0100

    A bug related to the way applyingTransactionQueueingTime is fixed. Unaccessabile block of code in the certifyAndVote task is removed.

--- src/fr/inria/jessy/transaction/termination/ApplyTransactionsToDataStore.java
+++ src/fr/inria/jessy/transaction/termination/ApplyTransactionsToDataStore.java
@@ -5,16 +5,17 @@ import java.util.concurrent.LinkedBlockingQueue;
 import fr.inria.jessy.communication.message.TerminateTransactionRequestMessage;
 
 /**
- * This class is used for applying the transactions to the data store SEQUENTIALLY.
- * For certain consistencies (i.e., NMSI-GMUVector, US, SI, PSI), update transactions should be applied sequentially.  
+ * This class is used for applying the transactions to the data store
+ * SEQUENTIALLY. For certain consistencies (i.e., NMSI-GMUVector, US, SI, PSI),
+ * update transactions should be applied sequentially.
  * 
  * @author Masoud Saeida Ardekani
- *
+ * 
  */
 public class ApplyTransactionsToDataStore implements Runnable {
 
 	private LinkedBlockingQueue<TerminateTransactionRequestMessage> queue;
-	
+
 	private DistributedTermination distributedTermination;
 
 	public ApplyTransactionsToDataStore(
@@ -30,15 +31,11 @@ public class ApplyTransactionsToDataStore implements Runnable {
 
 				TerminateTransactionRequestMessage msg = queue.take();
 
-				distributedTermination.applyingTransactionQueueingTime
-						.add(System.nanoTime()
-								- msg.getExecutionHistory()
-										.getApplyingTransactionQueueingStartTime());
+				DistributedTermination.measureApplyingTransactionQueueingTime(msg);
 
 				distributedTermination.handleTerminationResult(msg);
 
-				distributedTermination.measureCertificatioinTime(msg, msg
-						.getExecutionHistory().isVoteReceiver());
+				DistributedTermination.measureCertificationTime(msg);
 
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -51,8 +48,8 @@ public class ApplyTransactionsToDataStore implements Runnable {
 		queue.add(msg);
 
 	}
-	
-	public void removeFromQueue(TerminateTransactionRequestMessage msg){
+
+	public void removeFromQueue(TerminateTransactionRequestMessage msg) {
 		queue.remove(msg);
 	}
 }
