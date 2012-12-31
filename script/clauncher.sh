3252bdd6cc28f5e2140f9e88d83cb4db567d89ac
Masoud Saeida Ardekani <db.oracle@gmail.com>
2012-12-31 16:03:17 +0100

    Some bugs related to the jessy scripts are fixed, and some enhancements are added.
    1. clauncher now contains a timer that should be set in the configuration.sh file. Upon timeout, current execution of clauncher is terminated, and experience.sh re-run again clauncher without changing the parameters. This loop continues until clauncher finishes successfully or be interrupted by a termination signal.
    
    2. Both loading and execution probes were gathered in the final output file of client.sh . The change now get rid of loading phase probes (that are useless).
    
    3. Some bugs related to grid5000 deployment are fixed. The bugs prohibited the correct execution of a scenario because of synchronization problems. Moreover, the final results were not synchronized at the main frantal correctly. Both these issues are fixed by adding two functions to the experience.sh that only run in case of a grid5000 deployment.
    
    4. Some new outputs are added to the experience.sh collectStats function.
    
    5. A new script for killing the clauncher and re-execute it is added. This script kills the current clauncher job, thus experience re-run it with the same configurations. It is useful if the user notice the system is blocked, and the timeout is set to a very large value. The file is called killclauncher.sh
    
    Conflicts:
    	script/jessy.jar

--- script/clauncher.sh
+++ script/clauncher.sh
@@ -9,14 +9,19 @@ function stopExp(){
     let e=${#clients[@]}-1
     for i in `seq 0 $e`
     do
-	echo "stopping on ${clients[$i]}"
-	nohup ${SSHCMD} ${clients[$i]} "killall -SIGTERM java" 2&>1 > /dev/null &
+        echo "stopping on ${clients[$i]}"
+        nohup ${SSHCMD} ${clients[$i]} "killall -SIGTERM java" 2&>1 > /dev/null &
     done
 
 }
 
-trap "stopExp; wait; exit 255" SIGINT SIGTERM
+function stopTimer(){
+        kill $TIMERPID
+}
+trap "echo 'Clauncher catches Quit Signal'; stopTimer; stopExp; wait; exit 255" SIGINT SIGTERM
 
+#Trap timeout
+trap "echo 'Clauncher catches Timeout'; stopExp; wait; exit 3" SIGUSR1
 
 # 2 - Experimentation
 
@@ -25,7 +30,22 @@ for i in `seq 0 $e`
 do
     echo "launching on ${clients[$i]}"
     nohup ${SSHCMD} ${clients[$i]} "${scriptdir}/client.sh" > ${clients[$i]} &
+    CLIENTPID[$i]=$!
 done
 
-wait
+export CLIENTPID
 
+export CLAUNCHERPID=$$
+
+#Run the timeout. clauncherTimeout should be set in the configuratio file.
+(sleep $clauncherTimeout ; kill -SIGUSR1 $CLAUNCHERPID) &
+
+TIMERPID=$!
+export TIMERPID
+
+#Wait normally for all clients to finish
+wait ${CLIENTPID[*]}
+echo "Clauncher finishes successfully. Returning to experience."
+
+stopTimer
+exit 0
